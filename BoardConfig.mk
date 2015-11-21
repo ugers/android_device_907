085f7d160ea6362b452c5ac0717550409ee44067
fire855 <thefire855@gmail.com>
2015-11-21 13:27:52 +0100

    Use rtl8192cu driver

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,11 +136,11 @@ BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_rtl
-BOARD_WLAN_DEVICE                := rtl8188eu
-SW_BOARD_USR_WIFI                := rtl8188eu
+BOARD_WLAN_DEVICE                := rtl8192cu
+SW_BOARD_USR_WIFI                := rtl8192cu
 
-WIFI_DRIVER_MODULE_NAME   := "8188eu"
-WIFI_DRIVER_MODULE_PATH   := "/system/lib/modules/8188eu.ko"
+WIFI_DRIVER_MODULE_NAME   := "8192cu"
+WIFI_DRIVER_MODULE_PATH   := "/system/lib/modules/8192cu.ko"
 
 WIFI_DRIVER_MODULE_ARG    := ""
 WIFI_FIRMWARE_LOADER      := ""
